8f35cd4a3c9e2759c0493784a10ea7ba8b4f2bad
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-05-28 16:17:51 +0900

    05-28

--- react/task-app/src/ex09/ex09.jsx
+++ react/task-app/src/ex09/ex09.jsx
@@ -5,12 +5,18 @@
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import { FilterButton } from "./TaskItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Ex09() {
     const [Input, setInput] = useState("");
     const [Tasks, setTask] = useState([]);
     const [Filter, setFilter] = useState("all")
+
+    useEffect(() => {
+        fetch("http://localhost:8000/tasks")    // fastapiオリジンからタスクリストをフェッチ
+            .then(res => res.json())            // レスポンスをjsonに変換
+            .then(data => setTask(data))        // fetchしたタスクを、このオリジンに取り込み
+    }, [])                                      // 第二引数はから配列、これでマウント時のみ実行
 
     // フィルター状態によってタスク配列を制御
     let FilteredTasks;
---
+++ react/task-app/src/ex10/TaskItem.jsx
@@ -0,0 +1,57 @@
+// 追加・削除・完了・フィルタ・リスト
+import { useState } from "react";
+
+//formの追加ボタンのロジック
+function AddButton({tasks, set_task, input, set_input}) {
+    const handleAdd = () => {
+        if (input.trim() === "") return;
+        const new_task = {id: Date.now(), text: input, complete: false};
+        set_task([...tasks, new_task]);
+        set_input("");
+    };
+
+    return <button onClick={handleAdd}>追加</button>
+}
+
+function DeleteButton({tasks, set_task, id}) {
+    const handleDelete = () => {
+        set_task(tasks.filter(
+            task => task.id !== id
+        ));
+    };
+
+    return <button onClick={handleDelete}>削除</button>;
+}
+
+function CompButton({tasks, set_task, id}) {
+    const handleComp = () => {
+        set_task(tasks.map(task =>
+            task.id === id
+            ? {...task, complete: !task.complete}
+            : task
+        ));
+    };
+
+    return <button onClick={handleComp}>完了</button>;
+}
+
+function FilterButton({filter, set_filter}) {
+    return (
+        <>
+            <button onClick={() => set_filter("all")}>全て</button>
+            <button onClick={() => set_filter("active")}>未完了</button>
+            <button onClick={() => set_filter("complete")}>完了</button>
+        </>
+    )
+}
+
+// リスト内のロジック
+//
+function TaskItem({task, tasks, set_tasks}) {
+    const [IsEditing, setIsEditing] = useState(false);
+    const [EditText, setEditText] = useState
+
+    return (
+        {}
+    )
+}
