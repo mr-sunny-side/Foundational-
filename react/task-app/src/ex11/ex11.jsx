96b7585f84fc24243094cb0d4cf7cbbd2c9f4bf7
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-06-01 20:41:56 +0900

    06-01

--- react/task-app/src/ex11/ex11.jsx
+++ react/task-app/src/ex11/ex11.jsx
@@ -1,3 +1,19 @@
 // 05-30
 // ex11の記述
 // - fastAPIからタスクの初期値(get)を受け取る記述
+import { useState } from "react";
+
+import { FilterButton } from "./TaskItem";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
+
+function Ex11() {
+    const [Tasks, setTask] = useState([]);
+    const [Input, setInput] = useState("");
+    const [Filter, setFilter] = useState("all");
+
+    let FilteredTasks;
+    if (Filter === "active") {
+        FilteredTasks = Tasks.filter(task => !task.complete)
+    }
+}
