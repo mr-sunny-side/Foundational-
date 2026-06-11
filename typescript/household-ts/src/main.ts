c51afd635ac546c21a3233f03ca263f3a2b604c2
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-06-12 03:44:56 +0900

    06-12

--- typescript/household-ts/src/main.ts
+++ typescript/household-ts/src/main.ts
@@ -1,16 +1,5 @@
-function taxValue(value: number, tax: number): number {
-    return value * (1 + tax);
+import { Transaction } from "./types";
+
+function main() {
+
 }
-
-function formatItem(name: string, value: number): string{
-    return `${name}: ¥${value}`;
-}
-
-function main(): any {
-    const value: number = taxValue(300, 0.1);
-    const format: string = formatItem("apple", value);
-
-    console.log(format);
-}
-
-main()
---
+++ typescript/household-ts/src/types.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+    id: number;
+    amount: number;
+    label: string;
+    date: string;
+}
