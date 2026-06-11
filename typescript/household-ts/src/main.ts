744b513107d4b998863d1f516aafdf7408e61336
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-06-12 04:52:48 +0900

    06-12

--- typescript/household-ts/src/main.ts
+++ typescript/household-ts/src/main.ts
@@ -1,6 +1,32 @@
 import type { Transaction } from "./types";
-// 型情報をインポートするときはtypeをつける
+import type { transType } from "./types";
+
+function getSign(type: transType) {
+    switch (type) {
+        case "income":
+            console.log("is income.")
+            return 1;
+        case "expense":
+            console.log("is expense.")
+            return -1;
+        default:
+            console.log("type error")
+            return 0;
+    }
+}
 
 function main() {
-    const data: Transaction[] =
+    const data: Transaction[] = [
+        {type: "expense", id: 1, amount: 300, label: "apple", date: "2026-06-01"},
+        {type: "expense", id: 2, amount: 250, label: "banana", date: "2026-06-02"},
+        {type: "income", id: 3, amount: 1000, label: "pocket money", date: "2026-06-03"}
+    ];
+
+    for (const d of data) {
+        console.log(d);
+        console.log(getSign(d.type));
+    }
+
 }
+
+main()
--- typescript/household-ts/src/types.ts
+++ typescript/household-ts/src/types.ts
@@ -1,4 +1,7 @@
+export type transType = "income" | "expense";
+
 export interface Transaction {
+    type: transType;
     id: number;
     amount: number;
     label: string;
