0831fbcefee6a550e0fff1adad5a9e2b5b61e052
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-06-15 02:55:43 +0900

    06-15

--- js_info/ex02/main.js
+++ js_info/ex02/main.js
@@ -1,1 +1,16 @@
 // ex02から
+
+// 座標がグリッド内か返す
+function IsInBounds(row, col, rows, cols) {
+    const inRow = rows - row;
+    const inCol = cols - col;
+
+    // 列が0以下の場合を最初に弾く
+    if (row <= 0 || col <= 0) {
+        return false;
+    } else if (inRow < 0 || inCol < 0) { // 座標が大きすぎる場合を弾く
+        return false;
+    } else {
+        return true;
+    };
+}
