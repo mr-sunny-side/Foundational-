c0dd2fb3749d0702c853ae237991bda600fe4a7a
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-06-15 02:28:44 +0900

    06-15

---
+++ js_info/ex01/main.js
@@ -0,0 +1,14 @@
+const ROWS = 9;
+const COLS = 9;
+let MINE_COUNT = 10;
+
+const GAME_TITLE = "マインスイーパー";
+let IS_STARTED = false;
+
+console.log(typeof ROWS);
+console.log(typeof COLS);
+console.log(typeof MINE_COUNT);
+console.log(typeof GAME_TITLE);
+console.log(typeof IS_STARTED);
+console.log()
+console.log(`${GAME_TITLE}: ${COLS}✕${ROWS} 地雷${MINE_COUNT}個`)
